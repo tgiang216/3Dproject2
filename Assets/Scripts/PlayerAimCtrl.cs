8c4e2b4b3ccdae044987e5adcf3df2a720f59ed6
Truong Giang <v.d.tr.giang@gmail.com>
2023-08-23 20:27:28 +0700

    aim 3

--- Assets/Scripts/PlayerAimCtrl.cs
+++ Assets/Scripts/PlayerAimCtrl.cs
@@ -3,12 +3,15 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAimCtrl : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _aimVirtualCamera;
     [SerializeField] private float _normalSensitivity;
     [SerializeField] private float _aimSensitivity;
+    [SerializeField] private LayerMask _aimlayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
     private StarterAssetsInputs starterAssetsInput;
     private ThirdPersonController thirdPersonController;
     private void Awake()
@@ -19,16 +22,32 @@ public class PlayerAimCtrl : MonoBehaviour
 
     private void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, _aimlayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
         if (starterAssetsInput.aim)
         {
             _aimVirtualCamera.gameObject.SetActive(true);
             thirdPersonController.SetSensivity(_aimSensitivity);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDiection = (worldAimTarget - transform.position).normalized;
+            transform.forward = Vector3.Lerp(transform.forward, aimDiection, Time.deltaTime * 20f);
         }
         else
         {
             _aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.SetSensivity(_normalSensitivity);
         }
+
+        
     }
 
     public void Test(Input value)

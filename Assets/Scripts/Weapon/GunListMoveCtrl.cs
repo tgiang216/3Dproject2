96ae8c075f514fd5f0dcb1cd4b59ef4aa8321200
DESKTOP-SRLF03V\TG <v.d.tr.giang@gmail.com>
2023-08-28 06:42:28 +0700

    [Add] Dotween tesst - move the gun

--- Assets/Scripts/Weapon/GunListMoveCtrl.cs
+++ Assets/Scripts/Weapon/GunListMoveCtrl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GunListMoveCtrl : MonoBehaviour
 {
@@ -9,14 +10,31 @@ public class GunListMoveCtrl : MonoBehaviour
     [SerializeField] private float _radius;
     [SerializeField] private float _angle;
 
+    [SerializeField] PathType pathType = PathType.CatmullRom;
+    [SerializeField] Vector3[] waypoints = new[] {
+		new Vector3(4, 2, 0),
+		new Vector3(8, 6, 14),
+		new Vector3(4, 6, 14),
+		new Vector3(0, 6, 6),
+		new Vector3(-3, 0, 0)
+
+    }; 
     private void Start()
     {
-        //guns[1].transform.position = transform.position + new Vector3(Mathf.Cos(30f), Mathf.Sin(30f), 0) * 0.5f;
-        guns[1].transform.position = GetPositionOnCircle(_radius, _angle);
+        MoveAround(guns[1].transform, 1f);
     }
     private Vector3 GetPositionOnCircle(float radius, float angle)
     {
         return transform.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
        
+    }
+
+    private void MoveAround(Transform target, float radius)
+    {
+        Tween t = target.DOPath(waypoints, 4, pathType)
+            .SetOptions(true)
+            .SetLookAt(0.001f);
+        // Then set the ease to Linear and use infinite loops
+        t.SetEase(Ease.Linear).SetLoops(-1);
     }
 }
